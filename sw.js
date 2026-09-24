034c6c8c17923137813cb52019f4255576bdee51
GageWood <43021569+GageWood@users.noreply.github.com>
2026-09-24 12:54:54 -0500

    Bump cache for calculator root fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="grab-haul-v19";
-const ASSETS=["./","./index.html","./app.css","./app.js?v=20260924-1","./photo-estimate-ui.js","./manifest.json","./icon.svg","./icon-192.svg","./icon-512.svg"];
+const CACHE="grab-haul-v20";
+const ASSETS=["./","./index.html","./app.css","./app.js?v=20260924-2","./photo-estimate-ui.js","./manifest.json","./icon.svg","./icon-192.svg","./icon-512.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
